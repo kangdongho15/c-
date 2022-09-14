5cb76372610f1e00e2f1db2482c4bac4399fbbca
kangdongho15 <112610207+kangdongho15@users.noreply.github.com>
2022-09-14 11:14:04 +0900

    Add files via upload

---
+++ 20220908/20220908_kdh_1/20220908_kdh_1/20220908_kdh_1.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+    /*
+       첫번째 작성한 c프로그램입니다.
+       2022.09.08
+   */
+
+    printf("Hello  !   \n\n");
+    printf("I am a Student. \n");
+    printf("%d\n", 1234);
+    printf("%d   %d \n", 10, 20);
+    return 1;
+}
---
+++ 20220908/20220908_kdh_2/20220908_kdh_2/20220908_kdh_2.cpp
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+    int num1 = 12;
+    int num2 = 12;
+    printf("num1 : %d \n", num1);
+    printf("num1++ : %d \n", num1++);
+    printf("num1 : %d \n", num1);
+    printf("num1++ : %d \n", num1++);
+    return 1;
+}
